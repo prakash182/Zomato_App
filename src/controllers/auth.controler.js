c4cbce60c587977d45cfbd34c2bafa846e3ee53b
prakash182 <prakashchandora18@gmail.com>
2026-01-23 12:41:51 +0530

    updated Auth.Controller file

--- src/controllers/auth.controler.js
+++ src/controllers/auth.controler.js
@@ -1,1 +1,106 @@
-const authModel =require('../models/user.model');
+const userModel = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+async function registerUser(req, res) {
+  try {
+    const { fullName, email, password } = req.body;
+
+    const isUserAlreadyExist = await userModel.findOne({ email });
+
+    if (isUserAlreadyExist) {
+      return res.status(400).json({
+        message: "User already exist",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await userModel.create({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.cookie("token", token, {
+      httpOnly: true,
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        _id: user._id,
+        fullName: user.fullName,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Registration failed",
+    });
+  }
+}
+
+async function loginUser(req, res) {
+  try {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(400).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.cookie("token", token, {
+      httpOnly: true,
+    });
+
+    res.status(200).json({
+      message: "User login successfully",
+      user: {
+        _id: user._id,
+        fullName: user.fullName,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Login failed",
+    });
+  }
+}
+
+function logoutUser(req, res) {
+  res.clearCookie("token");
+  res.status(200).json({
+    message: "User logout successfully",
+  });
+}
+
+module.exports = {
+  registerUser,
+  loginUser,
+  logoutUser,
+};
