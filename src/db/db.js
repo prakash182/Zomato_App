a600010c26a51410669e21988d3f5a22da04d752
prakash182 <prakashchandora18@gmail.com>
2026-02-03 23:08:39 +0530

    some changes done for final backend development

--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 function ConnectDB (){
-    mongoose.connect(process.env.MONGO_URI)
+    mongoose.connect(process.env.MONGODB_URI)
     .then(()=>{
         console.log("mongodb successfully connected!");
     })
--- src/routes/food.routes.js
+++ src/routes/food.routes.js
@@ -1,14 +1,44 @@
 const express = require('express');
 const foodController = require("../controllers/food.controller")
 const authMiddleware = require("../middlewares/auth.middleware")
+const router = express.Router();
 const multer = require('multer');
 
-const router = express.Router();
+
+const upload = multer({
+    storage: multer.memoryStorage(),
+})
 
 
-/* /api/food-partner/:id */
-router.get("/:id",
+/* POST /api/food/ [protected]*/
+router.post('/',
+    authMiddleware.authFoodPartnerMiddleware,
+    upload.single("mama"),
+    foodController.createFood)
+
+
+/* GET /api/food/ [protected] */
+router.get("/",
     authMiddleware.authUserMiddleware,
-    foodPartnerController.getFoodPartnerById)
+    foodController.getFoodItems)
 
-module.exports = router;
+
+router.post('/like',
+    authMiddleware.authUserMiddleware,
+    foodController.likeFood)
+
+
+router.post('/save',
+    authMiddleware.authUserMiddleware,
+    foodController.saveFood
+)
+
+
+router.get('/save',
+    authMiddleware.authUserMiddleware,
+    foodController.getSaveFood
+)
+
+
+
+module.exports = router
